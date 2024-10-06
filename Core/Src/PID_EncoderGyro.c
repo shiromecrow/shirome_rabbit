51c1db0ab9070af014b777ade15655f5f1903d6a
fukuda_shingo <shinshinmice@gmail.com>
2024-10-06 23:03:52 +0900

    PID構造整理

--- Core/Src/PID_EncoderGyro.c
+++ Core/Src/PID_EncoderGyro.c
@@ -55,88 +55,67 @@ void clear_Ierror(void) {
 void EncoderGyro_PID(float *PID_s, float *PID_t,float straight_velocity,float turning_velocity,float turning_displacement) {
 	float PID_stra = 0;
 	float PID_turn = 0;
-	if(highspeed_mode == 1){
-		Ksp = 3.9; //3//P項の制御量直進
-		Ksi = 0.06; //80//5//43//I項の制御量直進
-		Ksd = 0.001; //D項の制御量直進
-		Ktp = 4.4; //295//P項の制御量旋回
-		Kti = 0.142; //1//.6//I項の制御量旋回
-		Ktd = 3; //205//D項の制御量旋回
-	}else{
+	float obs_vel_str;//直線観測値
+	float obs_vel_turn;//旋回観測値
+	float obs_angle_turn;//旋回観測値
+
+
+
+	if(highspeed_mode == 0){//探索用
 		Ksp = 3.8; //3//P項の制御量直進
 		Ksi = 0.10; //80//5//43//I項の制御量直進
 		Ksd = 0.00; //D項の制御量直進
 		Ktp = 2.8; //295//P項の制御量旋回
 		Kti = 0.122; //1//.6//I項の制御量旋回
 		Ktd = 0.002; //205//D項の制御量旋回
-	}
-	if(modeacc == 6){
-		Ksp = 4.7; //3//P項の制御量直進
-		Ksi = 0.13; //80//5//43//I項の制御量直進
+		Ktp_angle = 0.0; //P項の制御量旋回
+		Ktd_angle = 0.0; //D項の制御量旋回
+		if(modeacc == 2){
+			Ktp = 3.3; //295//P項の制御量旋回
+			Kti = 0.13; //1//.6//I項の制御量旋回
+			Ktd = 0.02; //205//D項の制御量旋回
+			Ktp_angle = 250; //P項の制御量旋回
+			Ktd_angle = 1; //D項の制御量旋回
+		}else if(modeacc == 4){
+			Ktp_angle = 150; //P項の制御量旋回
+			Ktd_angle = 1; //D項の制御量旋回
+		}
+		obs_vel_str = (fusion_speedR + fusion_speedL) / 2;
+		obs_vel_turn = angle_speed;
+		obs_angle_turn=yaw_angle;
+	}else if(highspeed_mode == 1){//最短用
+		Ksp = 3.9; //3//P項の制御量直進
+		Ksi = 0.06; //80//5//43//I項の制御量直進
 		Ksd = 0.001; //D項の制御量直進
-	}
-
-//	if(straight_velocity>3000){
-//		Ksp = 2.1; //3//P項の制御量直進
-//		Ksi = 0.08; //80//5//43//I項の制御量直進
-//		Ksd = 0.001; //D項の制御量直進
-//	}
-
-	if (straight_velocity > 500 && highspeed_mode == 1) {//straight_velocity > 1400 && modeacc == 1 && ターン調整時にたまに角度ずれが発生のため常にON
-		//Ktp = 3.0; //P項の制御量旋回
-		//Kti = 0.06; //I項の制御量旋回
-		//Ktd = 0.009; //D項の制御量旋回
+		Ktp = 4.3; //295//P項の制御量旋回
+		Kti = 0.132; //1//.6//I項の制御量旋回
+		Ktd = 0.1; //205//D項の制御量旋回
 		Ktp_angle = 30; //P項の制御量旋回
 		Ktd_angle = 1; //D項の制御量旋回
 		if(g_WallControl_mode >= 1){
-			Ktp_angle = 150; //P項の制御量旋回
-			Ktd_angle = 4; //D項の制御量旋回
+			Ktp_angle = 50; //P項の制御量旋回
+			Ktd_angle = -4; //D項の制御量旋回
 		}
-	}else{
-		Ktp_angle = 0.0; //P項の制御量旋回
-		Ktd_angle = 0.0; //D項の制御量旋回
+		if(modeacc == 6){
+			Ksp = 4.7; //3//P項の制御量直進
+			Ksi = 0.13; //80//5//43//I項の制御量直進
+			Ksd = 0.001; //D項の制御量直進
+			Ktp = 4.8; //295//P項の制御量旋回
+			Kti = 0.132; //1//.6//I項の制御量旋回
+			Ktd = 3; //205//D項の制御量旋回
+			Ktp_angle = 30; //P項の制御量旋回
+			Ktd_angle = 1; //D項の制御量旋回
+		}
+		if (straight_velocity < 1800){
+			obs_vel_str = (fusion_speedR + fusion_speedL) / 2;
+		}else{
+			obs_vel_str = 0.5*kalman_speed + 0.5*(fusion_speedR + fusion_speedL) / 2;
+		}
+		obs_vel_turn = angle_speed;
+		obs_angle_turn=yaw_angle;
 	}
-	if(modeacc == 2){
-		Ktp = 3.3; //295//P項の制御量旋回
-		Kti = 0.13; //1//.6//I項の制御量旋回
-		Ktd = 0.02; //205//D項の制御量旋回
-		Ktp_angle = 750; //P項の制御量旋回
-		Ktd_angle = 1; //D項の制御量旋回
-	}else if(modeacc == 4){
-		Ktp_angle = 350; //P項の制御量旋回
-		Ktd_angle = 1; //D項の制御量旋回
-	}
 
-
-//	if(Encoder_PID_mode==0){
-//		Ksp = 0.0; //3//P項の制御量直進
-//		Ksi = 0.0; //80//5//43//I項の制御量直進
-//		Ksd = 0.0; //D項の制御量直進
-//	}
-//	if(Gyro_PID_mode==0){
-//		Ktp = 0.0; //295//P項の制御量旋回
-//		Kti = 0.0; //1//.6//I項の制御量旋回
-//		Ktd = 0.0; //205//D項の制御量旋回
-//		Ktp_angle = 0.0; //P項の制御量旋回
-//		Ktd_angle = 0.0; //D項の制御量旋回
-//	}
-
-//	if (straight_velocity == 0) {
-//		reset_speed();
-//	}
-	//straight.velocity>=2500 && fabs(angle_speed)<100
-//	if (fabs(angle_speed) < 120 && straight_velocity >= 100 && modeacc!=4) {// && Accel_PID_mode==1
-//		enc.error = (straight_velocity - (fusion_speedR + fusion_speedL) / 2);
-//	} else {
-//		enc.error = (straight_velocity - (E_speedR + E_speedL) / 2);
-//		fusion_speedL = E_lpf_speedL;
-//		fusion_speedR = E_lpf_speedR;
-//	}
-//	if (straight_velocity >= 1800) {
-//	enc.error = (straight.velocity - kalman_speed);
-//	}
-
-	enc.error = (straight_velocity - (fusion_speedR + fusion_speedL) / 2);
+	enc.error = straight_velocity - obs_vel_str;
 	enc.delta_error = enc.error - enc.old_error;
 	enc.old_error = enc.error;
 	enc.sigma_error += enc.error;
@@ -145,7 +124,7 @@ void EncoderGyro_PID(float *PID_s, float *PID_t,float straight_velocity,float tu
 
 
 
-	Gyro.error = (turning_velocity - angle_speed);
+	Gyro.error = turning_velocity - obs_vel_turn;
 	Gyro.delta_error = Gyro.error - Gyro.old_error;
 	Gyro.old_error = Gyro.error;
 	Gyro.sigma_error += Gyro.error;
@@ -153,7 +132,7 @@ void EncoderGyro_PID(float *PID_s, float *PID_t,float straight_velocity,float tu
 			+ Ktd * Gyro.delta_error;
 
 
-	Gyro_angle.error = turning_displacement - yaw_angle;
+	Gyro_angle.error = turning_displacement - obs_angle_turn;
 	Gyro_angle.delta_error = Gyro_angle.error - Gyro_angle.old_error;
 	Gyro_angle.old_error = Gyro_angle.error;
 	PID_turn += Ktp_angle * Gyro_angle.error + Ktd_angle * Gyro_angle.delta_error;
