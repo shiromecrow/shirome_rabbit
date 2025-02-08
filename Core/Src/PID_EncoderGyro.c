331c0f9525823fb0432a115d20dc2941bbd12583
fukuda_shingo <shinshinmice@gmail.com>
2025-02-09 02:00:23 +0900

    PID調整(角度制御弱め)

--- Core/Src/PID_EncoderGyro.c
+++ Core/Src/PID_EncoderGyro.c
@@ -66,10 +66,10 @@ void EncoderGyro_PID(float *PID_s, float *PID_t,float straight_velocity,float tu
 		Ksi = 0.08; //80//5//43//I項の制御量直進
 		Ksd = 0.00; //D項の制御量直進
 		Ktp = 2.6; //295//P項の制御量旋回
-		Kti = 0.11; //1//.6//I項の制御量旋回
+		Kti = 0.1; //1//.6//I項の制御量旋回
 		Ktd = 0.001; //205//D項の制御量旋回
-		Ktp_angle = 150.0; //P項の制御量旋回
-		Ktd_angle = 0.6; //D項の制御量旋回
+		Ktp_angle = 70.0; //P項の制御量旋回
+		Ktd_angle = 0.1; //D項の制御量旋回
 		if(modeacc == 2 || modeacc == 9){//旋回
 			Ktp = 2.4; //295//P項の制御量旋回
 			Kti = 0.11; //1//.6//I項の制御量旋回
@@ -94,8 +94,8 @@ void EncoderGyro_PID(float *PID_s, float *PID_t,float straight_velocity,float tu
 		Ktp = 2.9; //295//P項の制御量旋回
 		Kti = 0.112; //1//.6//I項の制御量旋回
 		Ktd = 0.00; //205//D項の制御量旋回		
-		Ktp_angle = 150; //P項の制御量旋回
-		Ktd_angle = 0.8; //D項の制御量旋回
+		Ktp_angle = 90; //P項の制御量旋回
+		Ktd_angle = 0; //D項の制御量旋回
 		/*	
 		if(g_WallControl_mode >= 1){
 			Ktp_angle = 300; //P項の制御量旋回
@@ -109,8 +109,8 @@ void EncoderGyro_PID(float *PID_s, float *PID_t,float straight_velocity,float tu
 			Ktp = 3.9; //295//P項の制御量旋回
 			Kti = 0.112; //1//.6//I項の制御量旋回
 			Ktd = 0.001; //205//D項の制御量旋回
-			Ktp_angle = 90; //P項の制御量旋回
-			Ktd_angle = 0.4; //D項の制御量旋回
+			Ktp_angle = 30; //P項の制御量旋回
+			Ktd_angle = 0; //D項の制御量旋回
 		}
 		if (straight_velocity < 200){
 			obs_vel_str = (fusion_speedR + fusion_speedL) / 2;
